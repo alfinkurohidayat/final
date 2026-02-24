9b0670b7268cee47f93f20437c1a7bbbf7060f37
alfinkurohidayat <alfinhidayatpro18@gmail.com>
2026-02-24 17:03:05 +0700

    remove unnecessary files

--- server.js
+++ server.js
@@ -384,8 +384,12 @@ app.use((err, req, res, next) => {
 // START SERVER
 // ==============================
 
-const PORT = process.env.PORT || 5001;
-
+mongoose
+  .connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 30000,
+  })
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB error:", err));
 app.listen(PORT, () => {
   console.log("🚀 Server running on port", PORT);
 });
